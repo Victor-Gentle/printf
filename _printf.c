7dcaa0246a1a8c258595ff2ea361bb778314c532
godstime01 <youngestkid01@gmail.com>
2022-11-22 13:33:47 -0800

    added readme

--- _printf.c
+++ _printf.c
@@ -3,8 +3,9 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * _printf - Printf function.
  * @format: format.
+ *
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
